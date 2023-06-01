b47ed1b93ea35449a8e9530da85ddb8ce0e5a8aa
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-01 13:41:00 +0300

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,3 @@
-
-
-
 export function onFetch(name){
     return  fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(res => {
         if(!res.ok){
