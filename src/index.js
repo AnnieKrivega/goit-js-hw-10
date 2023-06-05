200b1ad3fb063f4c3b5dd69297f05256756349fd
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-05 19:58:21 +0300

    add

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function onInput(evt) {
               refs.ulEl.innerHTML = selCountries;
             }
             if(selCont.length === 1){
-              refs.countryEl.innerHTML = '';
+            
               refs.ulEl.innerHTML = '';
                 onFullCard(selCont)
                 
