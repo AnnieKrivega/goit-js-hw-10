edd72c746405cf62441c6709ebfa75f439347ded
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-05 22:34:42 +0300

    add

--- src/index.js
+++ src/index.js
@@ -55,8 +55,7 @@ function onInput(evt) {
       )
       .catch(error => {
         if(error.message === "404" ) {
-          refs.ulEl.innerHTML = '';
-          refs.countryEl.innerHTML = '';
+
           console.log(error); Notiflix.Notify.failure("Oops, there is no country with that name");
       } else { Notiflix.Notify.failure("error.message");
     }
