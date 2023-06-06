0d413bddf207d609ba1358d025355c2d851fe6f6
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-06-06 22:43:24 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,83 +1,3 @@
-// import './css/styles.css';
-// import Notiflix from 'notiflix';
-// import { onFetch } from './fetchCountries'
-// const DEBOUNCE_DELAY = 300;
-
-// import debounce from "lodash.debounce";
-
-// const refs ={
-//     inputEl: document.querySelector('#search-box'),
-//     ulEl: document.querySelector('.country-list'),
-//     countryEl: document.querySelector('.country-info')
-// }
-
-// refs.inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY, ));
-
-// function onInput(evt) {
-
-//     let inputText = refs.inputEl.value.trim() ;
-//     if(!inputText){
-//       refs.ulEl.innerHTML = '';
-//       refs.countryEl.innerHTML = '';
-//       return
-//   }
-
-//     evt.preventDefault()
-//     onFetch(inputText)
-//       .then(selCont => {
-//         console.log(selCont);
-//         if (selCont.length > 10) {
-//           refs.countryEl.innerHTML = '';
-//           refs.ulEl.innerHTML = '';
-//           console.log('too much');
-//           Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-//           return;
-//         }
-  
-//         let selCountries = '';
-//         if(selCont.length > 1){
-//             selCont.forEach(cont => {
-//                 refs.countryEl.innerHTML = '';
-//                 refs.ulEl.innerHTML = '';
-//                 selCountries += `<li style='display: flex; align-items: center; margin-bottom: 20px ' ><img style='width: 40px; margin-right:20px;  ' src='${cont.flags.svg}'></img><p>${cont.name.official}</p></li>`;
-//               });
-//               refs.ulEl.innerHTML = selCountries;
-//             }
-//             if(selCont.length === 1){
-//               refs.countryEl.innerHTML = '';
-//               refs.ulEl.innerHTML = '';
-//                 onFullCard(selCont)
-                
-//             }
-            
-//         }
-
-//       )
-//       .catch(error => {
-//         if(error.message === "404" ) {
-
-//           console.log(error); Notiflix.Notify.failure("Oops, there is no country with that name");
-//       } else { Notiflix.Notify.failure("error.message");
-//     }
-//       });
-  
-// function onFullCard(selCont){
-//     let selCountries = '';
-//     refs.ulEl.innerHTML = '';
-//     selCountries  += `<li style=' align-items: center; margin-bottom: 20px ' ><img style='width: 80px; margin-right:20px;  ' src='${selCont[0].flags.svg}'></img><h3>${selCont[0].name.official}</h3><div><p><b>Capital</b>: ${selCont[0].capital}</p><p><b>Population</b>: ${selCont[0].population}</p><p><b>Languages</b>: ${Object.values(selCont[0].languages).join(', ')}</p></div></li>`;
-//     refs.countryEl.innerHTML = selCountries;
-// }
-// $(document).ready(function() {
-//   $('.elemClass').click(function(event) {
-//   console.log(event.target.id);
-//   });
-//   });
-
-//   const myElement = document.getElementById('event.target.id'); 
-//   if (errorOccurred) {
-//     myElement.innerHTML = ''; 
-//   }}
-
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
@@ -115,7 +35,9 @@ function handlerCountry(event) {
       } else if (name === '') {
         infoCountry.innerHTML = ' ';
         listCountry.innerHTML = ' ';
-
+        inputCountry.style.boxShadow = '5px 5px 5px gray';
+        inputCountry.style.color = 'black';
+        //Notiflix.Notify.failure('hhhhh');
       } else {
         createFlagCountry(countries);
       }
@@ -123,6 +45,8 @@ function handlerCountry(event) {
     .catch(error => {
       console.error(error);
 
+      inputCountry.style.boxShadow = '5px 5px 5px red';
+      inputCountry.style.color = 'red';
       Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }
@@ -148,14 +72,14 @@ function createCardCountry(countries) {
     <ul class="list-of-properties">
       <li class="property-item"><p><b>Capital: </b>${capital}</p></li>
       <li class="property-item"></l<p><b>Population: </b>${population}</p></li>
-      <li class="property-item"><><b>Languages: </b>${Object.values(
+      <li class="property-item"><p><b>Languages: </b>${Object.values(
         languages
       )}</p></li>
       </ul>`;
       }
     )
     .join('');
-
+  //console.log(markup);
   infoCountry.innerHTML = markup;
 }
 function createFlagCountry(countries) {
@@ -171,6 +95,6 @@ function createFlagCountry(countries) {
     </div>`;
     })
     .join('');
-
+  //console.log(markup);
   listCountry.innerHTML = markup;
 }
